bc343b5e95e95705a15b365abb72c1f0588cf67a
windi fikriyansyah <diwin321@gmail.com>
2026-02-12 23:34:11 +0700

    sa

--- resources/views/template/sidebar.blade.php
+++ resources/views/template/sidebar.blade.php
@@ -9,15 +9,15 @@
 
     <li class="menu-item">
         <a href="{{ route('product.index') }}" class="menu-link">
-            <i class="menu-icon tf-icons bx bx-package"></i>
+            <i class="menu-icon tf-icons bx bx-box"></i>
             <div data-i18n="Products">Data Produk</div>
         </a>
     </li>
 
     <li class="menu-item">
         <a href="{{ route('blog.index') }}" class="menu-link">
-            <i class="menu-icon tf-icons bx bx-package"></i>
-            <div data-i18n="Products">Blog</div>
+            <i class="menu-icon tf-icons bx bx-news"></i>
+            <div data-i18n="Blog">Blog</div>
         </a>
     </li>
 
@@ -28,63 +28,63 @@
     </li>
     <li class="menu-item">
         <a href="javascript:void(0);" class="menu-link menu-toggle">
-            <i class="menu-icon tf-icons bx bx-collection"></i>
+            <i class="menu-icon tf-icons bx bx-layout"></i>
             <div data-i18n="Account Settings">Halaman Homepage</div>
         </a>
         <ul class="menu-sub">
 
             <li class="menu-item">
                 <a href="{{ route('hero.index') }}" class="menu-link">
-                    <i class="menu-icon tf-icons bx bx-image"></i>
+                    <i class="menu-icon tf-icons bx bx-slideshow"></i>
                     <div data-i18n="Hero Section">Hero Section</div>
                 </a>
             </li>
 
             <li class="menu-item">
                 <a href="{{ route('about.index') }}" class="menu-link">
-                    <i class="menu-icon tf-icons bx bx-image"></i>
-                    <div data-i18n="Hero Section">About Section</div>
+                    <i class="menu-icon tf-icons bx bx-info-square"></i>
+                    <div data-i18n="About Section">About Section</div>
                 </a>
             </li>
 
             <li class="menu-item">
                 <a href="{{ route('quality.index') }}" class="menu-link">
-                    <i class="menu-icon tf-icons bx bx-image"></i>
-                    <div data-i18n="Hero Section">Quality Section</div>
+                    <i class="menu-icon tf-icons bx bx-check-shield"></i>
+                    <div data-i18n="Quality Section">Quality Section</div>
                 </a>
             </li>
 
             <li class="menu-item">
                 <a href="{{ route('gallery.index') }}" class="menu-link">
-                    <i class="menu-icon tf-icons bx bx-image"></i>
-                    <div data-i18n="Hero Section">Gallery Section</div>
+                    <i class="menu-icon tf-icons bx bx-images"></i>
+                    <div data-i18n="Gallery Section">Gallery Section</div>
                 </a>
             </li>
 
             <li class="menu-item">
                 <a href="{{ route('why_choose.index') }}" class="menu-link">
-                    <i class="menu-icon tf-icons bx bx-image"></i>
-                    <div data-i18n="Hero Section">why choose us</div>
+                    <i class="menu-icon tf-icons bx bx-list-check"></i>
+                    <div data-i18n="WhyChooseUs">Why Choose Us</div>
                 </a>
             </li>
             <li class="menu-item">
                 <a href="{{ route('certificate.index') }}" class="menu-link">
-                    <i class="menu-icon tf-icons bx bx-image"></i>
-                    <div data-i18n="Hero Section">Certifikat Section</div>
+                    <i class="menu-icon tf-icons bx bx-certification"></i>
+                    <div data-i18n="Certificate Section">Certifikat Section</div>
                 </a>
             </li>
 
             <li class="menu-item">
                 <a href="{{ route('contact.index') }}" class="menu-link">
-                    <i class="menu-icon tf-icons bx bx-image"></i>
-                    <div data-i18n="Hero Section">Contact Section</div>
+                    <i class="menu-icon tf-icons bx bx-phone"></i>
+                    <div data-i18n="Contact Section">Contact Section</div>
                 </a>
             </li>
 
             <li class="menu-item">
                 <a href="{{ route('footer.index') }}" class="menu-link">
-                    <i class="menu-icon tf-icons bx bx-image"></i>
-                    <div data-i18n="Hero Section">Footer Section</div>
+                    <i class="menu-icon tf-icons bx bx-dock-bottom"></i>
+                    <div data-i18n="Footer Section">Footer Section</div>
                 </a>
             </li>
         </ul>
@@ -92,8 +92,8 @@
 
     <li class="menu-item">
         <a href="{{ route('about-page.index') }}" class="menu-link">
-            <i class="menu-icon tf-icons bx bx-package"></i>
-            <div data-i18n="Products">About Page</div>
+            <i class="menu-icon tf-icons bx bx-info-circle"></i>
+            <div data-i18n="AboutPage">About Page Content</div>
         </a>
     </li>
 
